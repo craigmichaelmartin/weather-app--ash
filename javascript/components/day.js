c3edf45a054d753386d48d76e78fc732bdf8decb
Craig Martin <cmartin@onshift.com>
2015-09-23 13:12:35 -0400

    add is-active class for selected day

--- javascript/components/day.js
+++ javascript/components/day.js
@@ -37,7 +37,10 @@ define([
         },
 
         getTemplateData: function () {
-            return _.extend({english: this.appState.get('scale') === 'english'}, this.model.attributes);
+            return _.extend({
+                english: this.appState.get('scale') === 'english',
+                isActive: this.appState.get('day') === this.model.get('day')
+            }, this.model.attributes);
         }
 
     });
