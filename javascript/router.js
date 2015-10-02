ab19cbbb7a122eff0b7642cb33f54a6905e0652e
Craig Martin <cmartin@onshift.com>
2015-10-02 12:34:31 -0400

    split router logic into more atomic (and testable) functions

--- javascript/router.js
+++ javascript/router.js
@@ -19,29 +19,41 @@ define([
             // This sets the attributes for the app state model.
             // It is only triggered on hard url changes,
             // and so is used for the first entry into the app.
-            this.on('route:loadApp', (function (zip, day, hour, scale) {
-                var values = {
-                    zip: +zip,
-                    day: +day || new Date().getUTCDate(),
-                    hour: hour && $.isNumeric(hour) ? +hour : scale && $.isNumeric(scale) ? +scale : void 0,
-                    scale: scale ? scale : hour && !$.isNumeric(hour) ? hour : 'english'
-                };
-                _.filter(values, function (val) {
-                    return val != void 0;
-                });
-                this.appState.set(values, {silent: true});
-            }).bind(this));
+            this.on('route:loadApp', this.loadApp.bind(this));
 
             // This is where the url and page title are kept in sync
             // with the application state.
-            this.appState.on('change', (function () {
-                var url = '' + this.appState.get('zip') +
-                          '/' + this.appState.get('day') +
-                          (this.appState.get('hour') ? ('/' + this.appState.get('hour')) : '') +
-                          '/' + this.appState.get('scale');
-                this.navigate(url);
-                document.title = 'Weather for ' + this.appState.get('zip') + ' on ' + this.appState.get('day')
-            }).bind(this));
+            this.appState.on('change', this.updatePeripheralsWithState.bind(this));
+        },
+
+        loadApp: function(zip, day, hour, scale) {
+            var values = this.getValues(zip, day, hour, scale);
+            this.appState.set(values, {silent: true});
+        },
+
+        getValues: function(zip, day, hour, scale) {
+            var values = {
+                zip: +zip,
+                day: +day || new Date().getUTCDate(),
+                hour: hour && $.isNumeric(hour) ? +hour : scale && $.isNumeric(scale) ? +scale : void 0,
+                scale: scale ? scale : hour && !$.isNumeric(hour) ? hour : day && !$.isNumeric(day) ? day : 'english'
+            };
+            // return _.filter(values, function (val) {
+            //     return val != void 0;
+            // });
+            return values;
+        },
+
+        updatePeripheralsWithState: function() {
+            this.navigate(this.getUrl());
+            document.title = 'Weather for ' + this.appState.get('zip') + ' on ' + this.appState.get('day')
+        },
+
+        getUrl: function() {
+            return '' + this.appState.get('zip') +
+                   '/' + this.appState.get('day') +
+                   (this.appState.get('hour') ? ('/' + this.appState.get('hour')) : '') +
+                   '/' + this.appState.get('scale');
         }
 
     });
