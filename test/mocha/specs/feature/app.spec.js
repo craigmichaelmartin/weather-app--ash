83575638f757e309c433031ba1a7a9635452d0cf
Craig Martin <cmartin@onshift.com>
2015-11-03 21:53:30 -0500

    add feature tests for app state
    Unfortunately (in respect to wanting to demo this), the state can only
    be invalidly set through the url - ie, there is no way in the ui to do
    so. That said, I've done validation on the url, and these are tests for
    when the router sets the url to the app state model.

--- test/mocha/specs/feature/app.spec.js
+++ test/mocha/specs/feature/app.spec.js
@@ -230,11 +230,98 @@ define(function(require) {
 
             describe('error messages', function() {
 
-                describe('for bad day input', function() {
+                describe('for zip input', function() {
 
-                    // TODO
-                    it('should indicate too many days into future', function() {
-                        //this.app.appState.set({day: 100}, {validate: true});
+                    afterEach(function() {
+                        $('.js-alertClose').click();
+                    });
+
+                    it('should indicate not valid zip', function() {
+                        expect(this.app.appState.set({zip: ''}, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.zipNotNumeric)).to.be.above(-1);
+                    });
+
+                    it('should indicate non-numeric zip', function() {
+                        expect(this.app.appState.set({zip: '1234S'}, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.zipNotNumeric)).to.be.above(-1);
+                    });
+
+                    it('should indicate too long zip', function() {
+                        expect(this.app.appState.set({zip: 123456}, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.zipNotLength)).to.be.above(-1);
+                    });
+
+                    it('should indicate too short zip', function() {
+                        expect(this.app.appState.set({zip: 1234}, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.zipNotLength)).to.be.above(-1);
+                    });
+
+                    it('should indicate negative zip', function() {
+                        expect(this.app.appState.set({zip: -1000}, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.zipNotLength)).to.be.above(-1);
+                    });
+
+                });
+
+                describe('for day input', function() {
+
+                    beforeEach(function() {
+                        this.getDate = Date.prototype.getDate;
+                        Date.prototype.getDate = sinon.stub().returns(25);
+                    });
+
+                    afterEach(function() {
+                        Date.prototype.getDate = this.getDate;
+                    });
+
+                    it('should indicate day is too far out', function() {
+                        expect(this.app.appState.set({ zip: 44147, day: 100 }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.dayNotNear)).to.be.above(-1);
+                    });
+
+                    it('should indicate day is too far out', function() {
+                        expect(this.app.appState.set({ zip: 44147, day: 15 }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.dayNotNear)).to.be.above(-1);
+                    });
+
+                });
+
+                describe('for hour input', function() {
+
+                    beforeEach(function() {
+                        this.getDate = Date.prototype.getDate;
+                        Date.prototype.getDate = sinon.stub().returns(25);
+                        this.getHours = Date.prototype.getHours;
+                        Date.prototype.getHours = sinon.stub().returns(12);
+                    });
+
+                    afterEach(function() {
+                        Date.prototype.getDate = this.getDate;
+                        Date.prototype.getHours = this.getHours;
+                    });
+
+                    it('should indicate cannot have hour without day', function() {
+                        expect(this.app.appState.set({ zip: 44147, day: void 0, hour: 13 }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.hourNeedsDay)).to.be.above(-1);
+                    });
+
+                    it('should indicate hour is not valid', function() {
+                        expect(this.app.appState.set({ zip: 44147, day: 25, hour: 100 }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.hourNotValid)).to.be.above(-1);
+                    });
+
+                    it('should indicate hour is not valid for today', function() {
+                        expect(this.app.appState.set({ zip: 44147, day: 25, hour: 10 }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.hourNotValidToday)).to.be.above(-1);
+                    });
+
+                });
+
+                describe('for scale input', function() {
+
+                    it('should return an error message for invalid scale', function() {
+                        expect(this.app.appState.set({ zip: 44147, scale: 'kelvin' }, {validate: true})).to.be.false;
+                        expect($('.js-alertText').text().indexOf(this.app.appState.scaleNotValid)).to.be.above(-1);
                     });
 
                 });
