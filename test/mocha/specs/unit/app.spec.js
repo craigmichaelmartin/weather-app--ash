b74f1940d702dd23a23930c419ab2faa1f5f1db9
Craig Martin <cmartin@onshift.com>
2015-11-04 21:55:56 -0500

    remove unnecessary nesting

--- test/mocha/specs/unit/app.spec.js
+++ test/mocha/specs/unit/app.spec.js
@@ -9,39 +9,31 @@ define(function(require) {
             expect(AppView).not.to.be.undefined;
         });
 
-        describe('purely', function() {
+        describe('the getConditionClass function', function() {
 
-            describe('the getConditionClass function', function() {
-                
-                it('should return is-rainy when rainy', function() {
-                    expect(AppView.prototype.getConditionClass('chance of rain')).to.equal('is-rainy');
-                    expect(AppView.prototype.getConditionClass('scattered showers')).to.equal('is-rainy');
-                    expect(AppView.prototype.getConditionClass('chance of thunderstorms')).to.equal('is-rainy');
-                });
-                
-                it('should return is-snowy when snowy', function() {
-                    expect(AppView.prototype.getConditionClass('chance of snow')).to.equal('is-snowy');
-                    expect(AppView.prototype.getConditionClass('3-4 inches of snow')).to.equal('is-snowy');
-                    expect(AppView.prototype.getConditionClass('heavy snowfall')).to.equal('is-snowy');
-                });
-                
-                it('should return is-clear when clear', function() {
-                    expect(AppView.prototype.getConditionClass('clear skies')).to.equal('is-clear');
-                    expect(AppView.prototype.getConditionClass('mostly sunny')).to.equal('is-clear');
-                    expect(AppView.prototype.getConditionClass('sunny')).to.equal('is-clear');
-                });
-                
-                it('should return is-cloudy as graceful default', function() {
-                    expect(AppView.prototype.getConditionClass('mostly cloudy')).to.equal('is-cloudy');
-                    expect(AppView.prototype.getConditionClass('who knows')).to.equal('is-cloudy');
-                    expect(AppView.prototype.getConditionClass('made up weathery words not expected')).to.equal('is-cloudy');
-                });
+            it('should return is-rainy when rainy', function() {
+                expect(AppView.prototype.getConditionClass('chance of rain')).to.equal('is-rainy');
+                expect(AppView.prototype.getConditionClass('scattered showers')).to.equal('is-rainy');
+                expect(AppView.prototype.getConditionClass('chance of thunderstorms')).to.equal('is-rainy');
             });
 
-        });
+            it('should return is-snowy when snowy', function() {
+                expect(AppView.prototype.getConditionClass('chance of snow')).to.equal('is-snowy');
+                expect(AppView.prototype.getConditionClass('3-4 inches of snow')).to.equal('is-snowy');
+                expect(AppView.prototype.getConditionClass('heavy snowfall')).to.equal('is-snowy');
+            });
 
-        describe('after being initialized', function() {
+            it('should return is-clear when clear', function() {
+                expect(AppView.prototype.getConditionClass('clear skies')).to.equal('is-clear');
+                expect(AppView.prototype.getConditionClass('mostly sunny')).to.equal('is-clear');
+                expect(AppView.prototype.getConditionClass('sunny')).to.equal('is-clear');
+            });
 
+            it('should return is-cloudy as graceful default', function() {
+                expect(AppView.prototype.getConditionClass('mostly cloudy')).to.equal('is-cloudy');
+                expect(AppView.prototype.getConditionClass('who knows')).to.equal('is-cloudy');
+                expect(AppView.prototype.getConditionClass('made up weathery words not expected')).to.equal('is-cloudy');
+            });
         });
     });
 });
